f41874771878139f52d3bf94f937d9aeed5538ce
Ibrahim-Salah69 <107865794+Ibrahim-Salah69@users.noreply.github.com>
2022-06-29 16:44:19 +0300

    code update

--- alarm system.py
+++ alarm system.py
@@ -3,19 +3,25 @@ from ultrasonic import Ultrasonic
 from ssd1306 import initialize, clear_oled
 from ssd1306_text import add_text
 
+
+ultrasonic_sensor = Ultrasonic()
+initialize()
+clear_oled()
+
 while True:
     distance_value = ultrasonic_sensor.measure_distance_cm()
     motion = pin2.read_digital()
 
-    add_text(0, 0, distance_value)
-    add_text(0, 1, "Hi")
-    add_text(0, 2, "------")
-    add_text(0, 3, "Meqdad")
+    add_text(0, 0, str(distance_value))
 
     if distance_value <= 20:
         if motion == 1:
+            add_text(0, 1, 'motion detected')
+
             pin1.write_digital(1)
             pin0.write_digital(1)
         else:
+            add_text(0, 1, 'no motion')
+            
             pin1.write_digital(0)
             pin0.write_digital(0)
